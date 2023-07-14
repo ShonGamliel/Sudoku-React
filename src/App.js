9aa5a229752ecf6dc5325c20362171692c1ff7c1
ShonGamliel <110163296+ShonGamliel@users.noreply.github.com>
2023-07-14 16:18:02 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,16 @@ import Box from "./Box";
 
 function App() {
   const [array, setArray] = React.useState(Array(81).fill(""));
+  const [startPositions, setStartPositions] = React.useState([]);
+
   const setNumberInArray = (position, newNumber) => {
+    if (!newNumber) {
+      return setArray((prevState) => {
+        let newarray = [...prevState];
+        newarray[position] = "";
+        return newarray;
+      });
+    }
     const lineRange = Array.from({ length: 9 }, (_, index) => index + Math.floor(position / 9) * 9);
     for (let ii of lineRange) {
       if (array[ii] == newNumber) {
@@ -34,6 +43,26 @@ function App() {
     return true;
   };
 
+  function generateArrayOfArrays(numArrays) {
+    var resultArray = [];
+
+    for (var i = 0; i < numArrays; i++) {
+      var num1 = Math.floor(Math.random() * 81);
+      var num2 = Math.floor(Math.random() * 9) + 1;
+      resultArray.push([num1, num2]);
+    }
+
+    return resultArray;
+  }
+
+  React.useEffect(() => {
+    let start = generateArrayOfArrays(10);
+    for (let i of start) {
+      setNumberInArray(i[0], i[1]);
+      setStartPositions((startPositions) => [...startPositions, i[0]]);
+    }
+  }, []);
+
   return (
     <div className="App">
       {[1, 2, 3, 4, 5, 6, 7, 8, 9].map((i1, idx1) => (
@@ -42,7 +71,7 @@ function App() {
             const position = idx1 * 9 + idx2;
             const columnGap = position % 3;
             const lineGap = (idx1 + 1) % 3;
-            return <Box key={position} position={position} setNumberInArray={setNumberInArray} style={{ borderBottomWidth: lineGap ? 0 : 3.5, borderLeftWidth: columnGap ? 0 : 3.5 }} />;
+            return <Box key={position} position={position} setNumberInArray={setNumberInArray} style={{ borderBottomWidth: lineGap ? 0 : 3.5, borderLeftWidth: columnGap ? 0 : 3.5 }} array={array} isStart={startPositions.includes(position)} />;
           })}
         </div>
       ))}
